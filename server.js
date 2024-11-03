2137e0454d66fcaece46d2dae15cde355b4c5bc8
giangtt03 <giangtt653@gmail.com>
2024-11-03 11:04:52 +0700

    config

--- server.js
+++ server.js
@@ -1,8 +1,41 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const favicon = require('express-favicon');
+
 const http = require('http');
 const { Server } = require('socket.io');
 
 const app = express();
+app.use(cors());
+
+dotenv.config();
+app.use(morgan('dev'));
+app.use(cookieParser());
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log('Db connected')
+}).catch((err)=> console.log(err));
+
+app.use(favicon(__dirname + '/public/fac.ico'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(methodOverride('_method'));
+
+//ghi log các yêu cầu HTTP
+app.use(morgan('dev'));
+
+// ghi log cookie
+app.use(cookieParser());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
