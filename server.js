1914747231ec057f69de942127377c7935cf0d02
giangtt03 <giangtt653@gmail.com>
2025-04-25 13:14:13 +0700

    fix

--- server.js
+++ server.js
@@ -68,5 +68,5 @@ app.use('/go', guestRoutes(io));
 app.use('/t', userRoutes);
 
 server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port:${PORT}`);
 });
